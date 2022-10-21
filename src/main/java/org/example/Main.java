cbf08c243a1cecacfa3786f6c068d2d33e4e4fba
SubanistaT <subanista0214@gmail.com>
2022-10-21 12:12:22 +0530

    2nd change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("It's my 1st maven project!...");
+        System.out.println("It's my 1st maven project!....");
         System.out.println ("changes can be added");
     }
 }
